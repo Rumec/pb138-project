6c52a172147655d49553bb2691e89273981badcb
Jan Seriš <jan.seris@seznam.cz>
2021-06-13 01:53:49 +0200

    copypaste get all components finished

--- DB-server/src/dataAccess/componentsLoader.ts
+++ DB-server/src/dataAccess/componentsLoader.ts
@@ -7,7 +7,39 @@ async function getCpus(db : PrismaClient)
 
 async function getCpusByCategory(db : PrismaClient, category: string)
 {
-    const cpus = await db.cPU.findMany(
+    return await db.cPU.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
+}
+
+async function getMotherboards(db : PrismaClient)
+{
+    return await db.motherboard.findMany();
+}
+
+async function getMotherboardsByCategory(db : PrismaClient, category: string)
+{
+    return await db.motherboard.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
+}
+
+async function getRAMs(db : PrismaClient)
+{
+    return await db.rAM.findMany();
+}
+
+async function getRAMsByCategory(db : PrismaClient, category: string)
+{
+    return await db.rAM.findMany(
         {
             where: {
                 category: category
@@ -17,51 +49,116 @@ async function getCpusByCategory(db : PrismaClient, category: string)
 }
 
 
-
-const getMotherboards = async (db : PrismaClient) =>
+async function getDisks(db : PrismaClient)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.disk.findMany();
 }
 
-const getRAMs = async (db : PrismaClient) =>
+async function getDisksByCategory(db : PrismaClient, category: string)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.disk.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
 }
 
-const getDisks = async (db : PrismaClient) =>
+async function getGPUs(db : PrismaClient)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.gPU.findMany();
 }
 
-const getGPUs = async (db : PrismaClient) =>
+async function getGPUsByCategory(db : PrismaClient, category: string)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.gPU.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
 }
 
-const getPSUs = async (db : PrismaClient) =>
+async function getPSUs(db : PrismaClient)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.pSU.findMany();
 }
 
-const getCases = async (db : PrismaClient) =>
+async function getPSUsByCategory(db : PrismaClient, category: string)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.pSU.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
 }
 
-const getMonitors = async (db : PrismaClient) =>
+async function getCases(db : PrismaClient)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.case.findMany();
 }
 
-const getKeyboards = async (db : PrismaClient) =>
+async function getCasesByCategory(db : PrismaClient, category: string)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.case.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
 }
 
-const getMouses = async (db : PrismaClient) =>
+async function getMonitors(db : PrismaClient)
 {
-    const cpus = await db.cPU.findMany();
+    return await db.screen.findMany();
 }
 
+async function getMonitorsByCategory(db : PrismaClient, category: string)
+{
+    return await db.screen.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
+}
+
+async function getKeyboards(db : PrismaClient)
+{
+    return await db.keyboard.findMany();
+}
+
+async function getKeyboardsByCategory(db : PrismaClient, category: string)
+{
+    return await db.keyboard.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
+}
+
+async function getMouses(db : PrismaClient)
+{
+    return await db.mouse.findMany();
+}
+
+async function getMousesByCategory(db : PrismaClient, category: string)
+{
+    return await db.mouse.findMany(
+        {
+            where: {
+                category: category
+            }
+        }
+    );
+}
 
 
